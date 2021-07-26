79ad80950115f03d03402c56a08f596d17510318
PavelOzhog <flashog@mail.ru>
2021-07-26 14:04:56 +0300

    Задание 2

--- Main.java
+++ Main.java
@@ -1,40 +1,84 @@
-package aplanaHomeTask2;
+package HomeTask2;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        SweetBox sweetBox = new SweetBox();
-
-        Candies candy1 = new Candies("Twix", 5.45, 1.68, 5464);
-        Candies candy2 = new Candies("Mars", 7.98, 2.88, 1231);
-        GingerBread gingerbread1 = new GingerBread("Tulsky", 8.98, 3.32, 9899);
-        GingerBread gingerbread2 = new GingerBread("Pokrovsky", 4.22, 9.99, 9878);
-        Marmelad marmelad1 = new Marmelad("Bears", 7.88, 9.32, 4322);
-        Marmelad marmelad2 = new Marmelad("Worms", 2.32, 5.43, 5322);
-
-        sweetBox.add(candy1, 0);           // добавление элемента в коробку
-        sweetBox.add(candy2, 1);
-        sweetBox.add(gingerbread1, 2);
-        sweetBox.add(gingerbread2, 3);
-        sweetBox.add(marmelad1, 4);
-        sweetBox.add(marmelad2, 5);
-
-
-        System.out.println("Вес коробки составляет " + sweetBox.getAllWeight());      //общий вес коробки
-        System.out.println((float) sweetBox.getAllCost());      //вся стоимость коробки
-
-        sweetBox.cosistOf();                                      // состав коробки
-
-
-
-
-         sweetBox.removeLast();             //удаление помледнего элеиента
-         sweetBox.removeByIndex(3);         //удаление по индексу
-
-   //     sweetBox.smartOptByCost(32.00);         //метод не доработан, приводит в бесконечному выполнению
-
-
+    static void findHighRankWord(TreeMap<String, Integer> map){
+        int highRank = 0;
+        ArrayList<String> list = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if (entry.getValue() > highRank){
+                list.clear();
+                list.add(entry.getKey());
+                highRank = entry.getValue();
+            }
+            else if (entry.getValue() == highRank){
+                list.add(entry.getKey());
+            }
+        }
+        System.out.println("Максимальная частота = " + highRank);
+        System.out.println("Слова с этой частотой:");
+        for(String word : list){
+            System.out.println(word);
+        }
     }
 
+    private static void showMap(TreeMap<String, Integer> map){
+        System.out.println("Статистика слов");
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+        System.out.println();
+    }
 
+    private static void showWords(TreeMap<String, Integer> map){
+        System.out.println("Отсортированный список слов");
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            for (int i = 0; i < entry.getValue(); i++) {
+                System.out.print(entry.getKey() + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите название файла: ");
+        String filename = scanner.nextLine();
+
+        if (new File(filename).exists()){
+            TreeMap<String, Integer> map = new TreeMap<>();
+
+            try(BufferedReader br = new BufferedReader(new FileReader(filename)))
+            {
+                String s;
+                while((s=br.readLine()) != null){
+                    String[] words = s.split("[^a-zA-Z]");
+                    for(String word : words){
+                        if (word.length() == 0)
+                            continue;
+                        word = word.toLowerCase();
+                        if (map.containsKey(word)){
+                            map.replace(word, map.get(word) + 1);
+                        }
+                        else {
+                            map.put(word, 1);
+                        }
+                    }
+                }
+            }
+            catch(IOException ex){
+                System.out.println(ex.getMessage());
+            }
+
+            showWords(map);
+            showMap(map);
+            findHighRankWord(map);
+        }
+        else{
+            System.out.println("Файл не найден");
+        }
+    }
 }
